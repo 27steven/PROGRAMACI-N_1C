9376e2753b6fd74d01fc9a4cad900f8cfd2a1f64
Steven Yanchatipan <marsstev@hotmail.com>
2023-10-04 10:26:02 -0500

    inicio

---
+++ HOLA_MUNDO.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (int argc, char* argv[]) { 
+    printf ("Hola Mundo");
+    return 0;
+}
+# include <stdio.h>
+
+int main (int argc, char *argv[] ){
+    printf ("hola mundo");
+    return 0;
+}
